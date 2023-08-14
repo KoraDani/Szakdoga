9ff84aa1d8161aedafaa556a0a1e0e91b984ae5c
KoraDani <59094464+KoraDani@users.noreply.github.com>
2023-08-14 17:19:40 +0200

    Delete bejelentkezes directory

--- bejelentkezes/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $conn = mysqli_connect('localhost','root','','okosotthon','3306');
-    if (!$conn) {
-        die("hiba a kapcsolódás során ".mysqli_connect_error());
-    }
-    if (!$conn -> set_charset("utf8")) {
-        echo "Karakterkodólás sikertelen";
-    }
--- bejelentkezes/kezdo.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-require_once("connect.php");
-echo file_get_contents('html/header.html');
-include('session.php');
-echo file_get_contents('html/main.html');
-echo file_get_contents('html/footer.html');
